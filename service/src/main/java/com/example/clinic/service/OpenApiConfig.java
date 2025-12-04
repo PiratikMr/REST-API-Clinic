7b558e9e78ae4c5293e77caf730c23184752c4c8
Alexandr <hitskov75@gmail.com>
2025-12-04 22:22:53 +0700

    Исправление окончания строк для gradlew

---
+++ service/src/main/java/com/example/clinic/service/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package com.example.clinic.service;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "API сервиса больницы",
+                description = "API для управления клиникой: доктора, пациенты, записи на прием"
+        ),
+        security = @SecurityRequirement(name = "Bearer Authentication")
+)
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer",
+        description = "Введите JWT токен доступа"
+)
+
+public class OpenApiConfig {}
--- service/src/main/java/com/example/clinic/service/api/dto/request/CreateDoctorRequest.java
+++ service/src/main/java/com/example/clinic/service/api/dto/request/CreateDoctorRequest.java
@@ -1,6 +1,7 @@
 package com.example.clinic.service.api.dto.request;
 
 import com.example.clinic.service.api.dto.request.objects.ScheduleDtoRequest;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -9,25 +10,53 @@ import lombok.Data;
 import java.util.List;
 
 @Data
+@Schema(description = "Запрос на создание нового доктора")
 public class CreateDoctorRequest {
     @NotBlank
+    @Schema(
+            description = "Логин для входа в систему",
+            example = "doctor_shon"
+    )
     private String login;
 
     @NotBlank
+    @Schema(
+            description = "Пароль пользователя",
+            example = "secure_password_1234"
+    )
     private String password;
 
     @NotBlank
+    @Schema(
+            description = "Имя доктора",
+            example = "Иван"
+    )
     private String firstName;
 
     @NotBlank
+    @Schema(
+            description = "Фамилия доктора",
+            example = "Иванов"
+    )
     private String lastName;
 
     @Nullable
+    @Schema(
+            description = "Отчество доктора",
+            example = "Иванович"
+    )
     private String middleName;
 
     @NotNull
+    @Schema(
+            description = "ID специальности",
+            example = "1"
+    )
     private Long specialtyId;
 
     @Nullable
+    @Schema(
+            description = "График работы"
+    )
     private List<ScheduleDtoRequest> schedules;
 }
